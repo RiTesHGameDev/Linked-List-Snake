3e499bebbfcbfeaa15bce3fd5fa425223422bc6c
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-11-09 12:58:02 +0530

    draw BodyPart

---
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -0,0 +1,51 @@
+#include "../include/Player/BodyPart.h"
+#include "../include/Global/Config.h"
+
+namespace Player
+{
+	using namespace Global;
+
+	BodyPart::BodyPart()
+	{
+		grid_position = sf::Vector2i(0, 0);
+		createBodyPartImage();
+	}
+	BodyPart::~BodyPart()
+	{
+		destroy();
+	}
+	void BodyPart::initialize(float width, float height, sf::Vector2i pos, Direction dir)
+	{
+		bodypart_width = width;
+		bodypart_height = height;
+		direction = dir;
+		grid_position = pos;
+
+		initializeBodyPartImage();
+	}
+	void BodyPart::initializeBodyPartImage()
+	{
+		bodypart_image->initialize(Config::snake_body_texture_path, bodypart_width, bodypart_height, getBodyPartScreenPosition());
+		bodypart_image->setOriginAtCentre();
+	}
+	void BodyPart::createBodyPartImage()
+	{
+		bodypart_image = new ImageView();
+	}
+	void BodyPart::destroy()
+	{
+		delete (bodypart_image);
+	}
+	void BodyPart::update()
+	{
+
+	}
+	void BodyPart::render()
+	{
+		bodypart_image->render();
+	}
+	sf::Vector2f BodyPart::getBodyPartScreenPosition()
+	{
+
+	}
+}
