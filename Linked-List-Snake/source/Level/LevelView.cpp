37049e0b27826dbfe6aa95bc39602b6cacf84669
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-11-13 11:38:14 +0530

    update

--- Linked-List-Snake/source/Level/LevelView.cpp
+++ Linked-List-Snake/source/Level/LevelView.cpp
@@ -33,22 +33,22 @@ namespace Level
 		background_rectangle->initialize(background_size, sf::Vector2f(0, 0), 0, background_color);
 		background_rectangle->show();
 	}
-	void LevelView::initializeBorder()
-	{
-		sf::RenderWindow* game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
-		sf::Vector2f border_size = sf::Vector2f(grid_width,grid_height);
-		sf::Vector2f border_position = sf::Vector2f(border_left_offset, border_top_offset);
-
-		border_rectangle->initialize(border_size, border_position, 
-			border_thickness, sf::Color::Transparent, border_color);
-		border_rectangle->show();
-	}
 	void LevelView::calculateGridExtents()
 	{
 		sf::RenderWindow* game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
 
 		grid_width = game_window->getSize().x - 2 * border_left_offset;
 		grid_height = game_window->getSize().y - 2 * border_top_offset;
+	}
+	void LevelView::initializeBorder()
+	{
+		sf::RenderWindow* game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+		sf::Vector2f border_size = sf::Vector2f(grid_width, grid_height);
+		sf::Vector2f border_position = sf::Vector2f(border_left_offset, border_top_offset);
+
+		border_rectangle->initialize(border_size, border_position,
+			border_thickness, sf::Color::Transparent, border_color);
+		border_rectangle->show();
 	}
 	void LevelView::update()
 	{
