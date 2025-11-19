a21ad4bd38e36de23dd2f5eda60e856e56325458
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-11-19 09:06:37 +0530

    insert at the end DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -40,5 +40,29 @@ namespace LinkedListLib
 		{
 
 		}
+
+		void DoubleLinkedList::insertNodeAtTail()
+		{
+			linked_list_size++;
+			Node* new_node = createNode();
+			Node* curr_node = head_node;
+
+			if (curr_node == nullptr)
+			{
+				head_node = new_node;
+				static_cast<DoubleNode*>(new_node)->previous = nullptr;
+				initializeNode(new_node, nullptr, Operation::TAIL);
+				return;
+			}
+
+			while (curr_node->next != nullptr)
+			{
+				curr_node = curr_node->next;
+			}
+
+			curr_node->next = new_node;
+			static_cast<DoubleNode*>(new_node)->previous = curr_node;
+			initializeNode(new_node, curr_node, Operation::TAIL);
+		}
 	}
 }
