bde77b407a5ff60da23c432a10d4aff4139573a6
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-11-19 17:08:49 +0530

    remove all node DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -217,6 +217,12 @@ namespace LinkedListLib
 
 		void DoubleLinkedList::removeAllNodes()
 		{
+			if (head_node == nullptr) return;
+
+			while (head_node != nullptr)
+			{
+				removeNodeAtHead();
+			}
 		}
 
 		void DoubleLinkedList::removeHalfNodes()
