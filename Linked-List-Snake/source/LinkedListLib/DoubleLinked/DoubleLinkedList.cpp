bed8de945c40c7a3d7ce2ee7c3b0de95155041d5
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-11-19 08:51:14 +0530

    insert at the beginning

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -15,5 +15,30 @@ namespace LinkedListLib
 		DoubleLinkedList::DoubleLinkedList() = default;
 
 		DoubleLinkedList::~DoubleLinkedList() = default;
+
+		void DoubleLinkedList::insertNodeAtHead()
+		{
+			linked_list_size++;
+			Node* new_node = createNode();
+
+			if (head_node == nullptr)
+			{
+				new_node = head_node;
+				static_cast<DoubleNode*>(new_node)->previous = nullptr;
+				initializeNode(new_node, nullptr, Operation::HEAD);
+				return;
+			}
+			
+			initializeNode(new_node, head_node, Operation::HEAD);
+			new_node->next = head_node;
+			static_cast<DoubleNode*>(head_node)->previous = new_node;
+
+			head_node = new_node;
+		}
+
+		void DoubleLinkedList::insertNodeAtMiddle()
+		{
+
+		}
 	}
 }
