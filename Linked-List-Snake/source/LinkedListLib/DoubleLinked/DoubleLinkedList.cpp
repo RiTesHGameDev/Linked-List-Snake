eef71c88ed8f7ab47b73a751ed233b1edc255c9d
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2025-11-19 14:27:24 +0530

    delete at the end DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -125,6 +125,31 @@ namespace LinkedListLib
 
 		}
 
+		void DoubleLinkedList::removeNodeAtTail()
+		{
+			if (head_node == nullptr) return;
+
+			linked_list_size--;
+
+			Node* cur_node = head_node;
+
+			if (cur_node->next == nullptr)
+			{
+				removeNodeAtHead();
+				return;
+			}
+
+			while (cur_node->next != nullptr)
+			{
+				cur_node = cur_node->next;
+			}
+
+			Node* previous = static_cast<DoubleNode*>(cur_node)->previous;
+			previous->next = nullptr;
+
+			delete (cur_node);
+		}
+
 		void DoubleLinkedList::removeNodeAtHead() {
 			linked_list_size--;
 
@@ -138,5 +163,7 @@ namespace LinkedListLib
 			cur_node->next = nullptr;
 			delete cur_node;
 		}
+
+
 	}
 }
